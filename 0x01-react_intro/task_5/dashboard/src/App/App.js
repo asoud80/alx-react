0dc9b6065c706d75d12f823a56e5e85fa38cd06a
asoud80 <amgad_a.soud@outlook.com>
2024-05-02 16:18:04 -0700

    0x07-react_redux_action_creator_normalizr

--- 0x01-react_intro/task_5/dashboard/src/App/App.js
+++ 0x01-react_intro/task_5/dashboard/src/App/App.js
@@ -1,5 +1,7 @@
-import logo from './holberton-logo.jpg';
+import React from 'react';
+import logo from '../assets/holberton-logo.jpg';
 import './App.css';
+import { getFullYear, getFooterCopy } from '../utils/utils';
 
 function App() {
 	return (
@@ -10,9 +12,22 @@ function App() {
 			</div>
 			<div className='App-body'>
 				<p>Login to access the full dashboard</p>
+				<section className='form-inputs'>
+					<section className='input'>
+						<label htmlFor='email'>Email:</label>
+						<input type='email' name='email' id='email' />
+					</section>
+					<section className='input'>
+						<label htmlFor='password'>Password: </label>
+						<input type='password' name='password' id='password' />
+					</section>
+					<button>OK</button>
+				</section>
 			</div>
 			<div className='App-footer'>
-				<p>Copyright 2020 - holberton School</p>
+				<p>
+					Copyright {getFullYear()} - {getFooterCopy(true)}
+				</p>
 			</div>
 		</div>
 	);
